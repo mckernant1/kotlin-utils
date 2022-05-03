36352da56559b42a7f4e22017254d511398b152d
Thomas McKernan <tmeaglei@gmail.com>
2022-05-03 15:42:40 -0700

    adding set operations

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.mckernant1"
-version = "0.0.12"
+version = "0.0.13"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/github/mckernant1/extensions/collections/CollectionExtensions.kt
+++ src/main/kotlin/com/github/mckernant1/extensions/collections/CollectionExtensions.kt
@@ -25,3 +25,21 @@ fun <T> Iterable<T>?.equals(other: Iterable<T>?, preserveOrder: Boolean = true):
 fun <T, R> Iterable<T>.mapParallel(transform: suspend (T) -> R): List<R> = runBlocking {
     return@runBlocking map { async { transform(it) } }.awaitAll()
 }
+
+fun <T> Iterable<T>.intersect(other: Iterable<T>): List<T> {
+    return this.filter { other.contains(it) }
+}
+
+fun <T> Iterable<T>.except(other: Iterable<T>): List<T> {
+    return this.filter { !other.contains(it) }
+}
+
+fun <T, K> Iterable<T>.intersectBy(other: Iterable<T>, selector: (T) -> K): List<T> {
+    val otherSet = other.map { selector(it) }.toHashSet()
+    return this.filter { otherSet.contains(selector(it)) }
+}
+
+fun <T, K> Iterable<T>.exceptBy(other: Iterable<T>, selector: (T) -> K): List<T> {
+    val otherSet = other.map { selector(it) }.toHashSet()
+    return this.filter { !otherSet.contains(selector(it)) }
+}
