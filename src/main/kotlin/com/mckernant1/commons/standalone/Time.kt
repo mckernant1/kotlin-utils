d1a73d2e485ce8af6371734c5e6af1515b227805
Thomas McKernan <tmeaglei@gmail.com>
2026-01-24 13:02:56 -0600

    add suspend support

--- src/main/kotlin/com/mckernant1/commons/standalone/Time.kt
+++ src/main/kotlin/com/mckernant1/commons/standalone/Time.kt
@@ -16,11 +16,41 @@ inline fun measureDuration(f: () -> Unit): Duration {
 }
 
 /**
+ * @param f the function to time
+ *
+ * @return the duration
+ */
+suspend inline fun measureDuration(
+    crossinline f: suspend () -> Unit
+): Duration {
+    val start = Instant.now()
+    f()
+    return start.elapsed()
+}
+
+/**
  * @param f function to time
  *
  * @return The timed operation containing the duration and result
  */
 inline fun <T> measureOperation(f: () -> T): TimedOperation<T> {
+    val start = Instant.now()
+    val result = f()
+    val elapsed = start.elapsed()
+    return TimedOperation(
+        elapsed,
+        result
+    )
+}
+
+/**
+ * @param f function to time
+ *
+ * @return The timed operation containing the duration and result
+ */
+suspend inline fun <T> measureOperation(
+    crossinline f: suspend () -> T
+): TimedOperation<T> {
     val start = Instant.now()
     val result = f()
     val elapsed = start.elapsed()
